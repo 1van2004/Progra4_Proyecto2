14724feeaff0d356e4d2d5475df0dcd6b22e3972
Iván Agüero Acosta <ivan092004@hotmail.com>
2025-05-23 22:12:55 -0600

    Creacion de API para tareas de fontaneros

---
+++ Water-SF/Data/TareaContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Water_SF.Data
+{
+    public class TareaContext : DbContext
+    {
+        public TareaContext(DbContextOptions<TareaContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+
+    public class Tarea
+    {
+        [Key]
+        public string Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string PerInCharge { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+    }
+
+    
+}
+
---
+++ Water-SF/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Water_SF.Data;
+using Water_SF.Services;
+
+namespace Water_SF
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        services.AddTransient<ITareasService, TareaService>();
+
+            services.AddDbContext<TareaContext>(options => options.UseInMemoryDatabase("tareadb"));
+            services.AddControllers();
+
+            // Add Swagger generation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Water-SF",
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API for Water-SF"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+
+
+        // Enable middleware to serve generated Swagger as a JSON endpoint.
+        app.UseSwagger();
+
+        // Enable middleware to serve swagger-ui, specifying the Swagger JSON endpoint.
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "TareaService API v1");
+        });
+    }
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+}
+    }
+}
+
